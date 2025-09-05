1632c286d2781bde349631738cdf727515c7bc84
Auroratide <tf.auroratide@gmail.com>
2025-09-05 15:37:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Codebase for initting Matchmaker doohickers.
+
+## Virtual Environment
+
+```
+# Setup
+python -m venv .venv
+
+# Start
+source .venv/bin/activate
+```
+
+## Init Pinecone database
+
+```
+python src/init-pinecone.py
+```
---
+++ src/init-pinecone.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from pinecone import Pinecone, ServerlessSpec
+
+load_dotenv()
+
+pinecone = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+
+INDEX_NAME = "matchmaker-interests"
+MODEL_DIMENSION = 768 # gemini-embedding-001
+
+if not pinecone.has_index(INDEX_NAME):
+	print("Index does not exist. Creating...")
+	pinecone.create_index(
+		name=INDEX_NAME,
+		vector_type="dense",
+		dimension=MODEL_DIMENSION,
+		metric="cosine",
+		spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+	)
+	print("Initialized vector db!")
+else:
+	print("Already initialized vector db.")
